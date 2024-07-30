1606831c56b91ddd75e23c36d0b83ca818860a18
kasperikpnn <kasperikpnn@gmail.com>
2024-07-30 13:17:32 +0300

    Added login and logout

--- app.py
+++ app.py
@@ -5,23 +5,6 @@ from sqlalchemy import text
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    result = db.session.execute(text("SELECT content FROM messages"))
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages) 
-
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = text("INSERT INTO messages (content) VALUES (:content)")
-    db.session.execute(sql, {"content":content})
-    db.session.commit()
-    return redirect("/")
+import routes
