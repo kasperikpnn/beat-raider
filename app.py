627df1a4f5aead029c9f0b389a9538545d196456
kasperikpnn <kasperikpnn@gmail.com>
2024-07-31 18:43:44 +0300

    song uploading, unique ID generation for songs

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
-from os import getenv
+import os
+from song_manager import SongManager
 
 app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
+app.secret_key = os.getenv("SECRET_KEY")
+SM = SongManager(upload_folder=os.path.join(app.root_path, 'uploads'))
 
 import routes
